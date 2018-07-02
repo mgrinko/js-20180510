77a7e2ac06d04e4e51896cb05d1d1bf869955692
ShishkinMA <allegro87sly@gmail.com>
2018-07-02 15:18:18 +0700

    add change image by click

--- scripts/phones/components/phone-viewer.js
+++ scripts/phones/components/phone-viewer.js
@@ -1,15 +1,28 @@
 import Component from '../../component.js';
 
 export default class PhoneViewer extends Component {
+  constructor({ element }) {
+    super({ element });
+    this.on('click', '[data-element="phone-thumbs"]', event => this._onClickImage(event));
+  }
+
   showPhone(phoneDetails) {
     this._render(phoneDetails);
     this.show();
   }
 
+  _onClickImage(event) {
+    if (event.target.tagName !== 'IMG') {
+      return;
+    }
+    let phoneImg = this._element.querySelector('[data-element="phone-img"]');
+    phoneImg.src = event.target.src;
+  }
+
   _render(phone) {
     console.log(phone);
     this._element.innerHTML = `
-      <img class="phone" src="${phone.images[0]}">
+      <img class="phone" src="${phone.images[0]}" data-element="phone-img">
 
       <button>Back</button>
       <button>Add to basket</button>
@@ -25,10 +38,7 @@ export default class PhoneViewer extends Component {
         .map(
           img => `
         <li>
-          <img src="${img}" 
-            data-element="phone-thumbs" 
-            data-phone-thumbs-src="${img}"
-          >
+          <img src="${img}"  data-element="phone-thumbs">
         </li>`,
         )
         .join('')}
