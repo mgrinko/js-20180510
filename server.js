c76b40d0f595a6d82f343d53de9679f78c99c1fa
ShishkinMA <allegro87sly@gmail.com>
2018-07-03 09:47:59 +0700

    add node-static

---
+++ server.js
@@ -0,0 +1,19 @@
+let http = require('http');
+var static = require('node-static');
+var file = new static.Server('.', {
+  cache: 0,
+});
+
+function accept(req, res) {
+  if (req.url.startsWith('/api')) {
+    setTimeout(() => {
+      file.serve(req, res);
+    }, 500);
+  } else {
+    file.serve(req, res);
+  }
+}
+
+http.createServer(accept).listen(3000);
+
+console.log('Server running on port 3000');
