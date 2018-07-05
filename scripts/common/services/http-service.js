41b3b30faccc5aa3d46ba794514781b906d81c07
mgrinko <glukym@gmail.com>
2018-07-05 22:15:27 +0300

    Base url was reverted

--- scripts/common/services/http-service.js
+++ scripts/common/services/http-service.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://mgrinko.github.io/js-20180510/api/';
-const BASE_URL = 'http://localhost:3000/api/';
+const BASE_URL = 'https://mgrinko.github.io/js-20180510/api/';
+// const BASE_URL = 'http://localhost:3000/api/';
 
 const HttpService = {
   sendRequest(url, {
