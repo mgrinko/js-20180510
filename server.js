685c41638594b68cfaf5d32af7df6370b18f4d50
ShishkinMA <allegro87sly@gmail.com>
2018-07-06 02:05:46 +0700

    change server headers

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ let http = require('http');
 var static = require('node-static');
 var file = new static.Server('.', {
   cache: 0,
+  headers: {
+    'Access-Control-Allow-Origin': 'https://artfulangel.github.io',
+    'Access-Control-Allow-Methods': 'GET,PUT',
+    'Access-Control-Allow-Headers': 'Content-Type',
+  },
 });
 
 function accept(req, res) {
